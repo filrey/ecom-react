a6aea25105468a2fa6c816fb8ff86f416107c0b1
Filiberto Reyes <filibertoreyes55@gmail.com>
2023-09-18 15:57:05 -0700

    Firebase db storage & category previews

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,15 +1,15 @@
-import { useContext } from "react";
-import { ProductsContext } from "../../contexts/products.context";
-import ProductCard from "../../components/product-card/product-card.component";
+import { Routes, Route } from "react-router-dom";
+
+import Category from "../category/category.component";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+
 import "./shop.styles.scss";
 const Shop = () => {
-  const { prodcuts } = useContext(ProductsContext);
   return (
-    <div className="products-container">
-      {prodcuts.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
 };
 export default Shop;
